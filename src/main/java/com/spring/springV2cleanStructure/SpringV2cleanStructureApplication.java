3fd6a3587f409fbbbfc2b9a50c86635fd4cc3cca
root046 <64629095+root046@users.noreply.github.com>
2023-10-03 12:52:56 +0300

    Initial commit

---
+++ src/main/java/com/spring/springV2cleanStructure/SpringV2cleanStructureApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springV2cleanStructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringV2cleanStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringV2cleanStructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springV2cleanStructure/SpringV2cleanStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springV2cleanStructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringV2cleanStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
